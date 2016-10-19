5fd89fc0d9156e11e9c53dca7920d13857c1a19e
lange <lange@141.19.155.57>
2016-10-19 13:28:41 +0200

    Der Code funktioniert. Falscheingaben sollten noch aussortiert werden.

--- GDI 1IB/src/de/hsMannheim/informatik/gdi/uebung03/VollkommeneZahl.java
+++ GDI 1IB/src/de/hsMannheim/informatik/gdi/uebung03/VollkommeneZahl.java
@@ -3,41 +3,36 @@ package de.hsMannheim.informatik.gdi.uebung03;
 import static gdi.MakeItSimple.*;
 
 public class VollkommeneZahl {
-			//Programm von Jan -> WhatsApp
+	// Bei einer vollkommenen Zahl ergibt die Summe der Teiler (außer der Zahl selbst) die Ausgangszahl.
+	// Beispiel: Die Zahl 6 hat die Teiler 1, 2 und 3; 1 + 2 + 3 = 6.
 
 	public static void main(String[] args) {
-	
-		println("Geben sie eine ganze Zahl ein: ");
-		int inputValue = -1;
-		String errorMassage = "Eingabe Ungültig";
 		
-		try {
-			inputValue = readInt();
+		println("Geben sie eine Zahl ein: ");
+		int ganzeZahl = readInt();
+		
+		int teiler = 1;
+		int summe = 0;
+		
+		while(teiler < ganzeZahl){
+			if(ganzeZahl % teiler == 0){
+				summe += teiler;
+			}
+			teiler ++;
 		}
-		catch(Exception ex) {
-			println(errorMassage);
-			return;
+		//Die while-Schleife rechnet die Summe der Teiler zusammen. 
+
+		
+
+		if (summe == ganzeZahl) {
+			println("ja, die eingegebene Zahl ist vollkommen");
+		} 
+		//Entspricht die Summe der Teiler, der Zahl die eingegeben wurde, gibt es eine positive Ausgabe.
+
+		else {
+			println("nein, die eingebenene Zahl ist nicht vollkommen");
 		}
 
-		if(inputValue == 0){
-			println(errorMassage);
-			return;
-		}
-		int count = 1;
-		int totalValue = 0;
-		while(count != inputValue){
-			if(inputValue % count == 0){
-				totalValue += count;
-			}
-			count++;
-		}
-		
-		if(totalValue == inputValue){
-			println("Ja, die eingegebene Zahl ist vollkommen");
-		}
-		else {
-			println("nein, die eingegebene Zahl ist nicht vollkommen");
-		}
 	}
 
 }
