7ff21a3f337c6cea60c45d61c154825051249e9e
1622998 <1622998@stud.hs-mannheim.de>
2016-10-30 11:33:05 +0100

    Eigene Tests und Notizen wurden erweitert

--- GDI 1IB/src/freierTest/BenTest.java
+++ GDI 1IB/src/freierTest/BenTest.java
@@ -5,9 +5,23 @@ import static gdi.MakeItSimple.*;
 public class BenTest {
 
 	public static void main(String[] args) {
-		
-		
-		println ((41 - 42) % 10);
+		// ALLES FORMATIEREN: alles markieren und <STRG> + <SHIFT> + <F>
+		// BEISPIEL KONKATENATION
+		String nachname, vorname, name;
+		nachname = "Schmidt";
+		vorname = "Hans";
+		name = vorname + " " + nachname;
+		println(name);
+
+		// BEISPIEL FÜR EINE IF-BEDINGUNG
+		println("Geben Sie Ihr Geburtsdatum ein: ");
+		String geburtsdatum = readLine();
+
+		if (geburtsdatum.equals("16.8."))
+			println("Wir haben am " + "gleichen Tag " + "Geburtstag!");
+
+		else
+			println("Wir haben nicht " + "am gleichen Tag " + "Geburtstag!");
 
 	}
 
--- GDI 1IB/src/freierTest/BenTest02.java
+++ GDI 1IB/src/freierTest/BenTest02.java
@@ -1,10 +1,21 @@
 package freierTest;
 
+import static gdi.MakeItSimple.*;
+
 public class BenTest02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x = 200, y = 4712;
+		println( max( 3, 5/2 ) );
+		int max = max( 2+3, 4 );
+		println( max( x, y ) );
 
 	}
 
+	static int max( int a, int b ) {
+		if ( a > b )
+		return a;
+		else
+		return b;
+		}
 }
