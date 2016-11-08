c0396868b02526414fe82612d1a7328aa970fb00
1622998 <1622998@stud.hs-mannheim.de>
2016-11-08 08:34:41 +0100

    Programm EightQueens wurde erstellt, aber noch nicht bearbeitet.

---
+++ GDI 1IB/src/de/hsMannheim/informatik/gdi/uebung06/EightQueens.java
@@ -0,0 +1,24 @@
+package de.hsMannheim.informatik.gdi.uebung06;
+
+import static gdi.MakeItSimple.*;
+
+public class EightQueens {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	static int[][] readFieldFromFile(String filename){
+		
+	}
+	
+	static boolean isValidSolution(int[][] field){
+		
+	}
+	
+	static void markThreatenedQueens(int[][] field){
+		
+	}
+	
+}
