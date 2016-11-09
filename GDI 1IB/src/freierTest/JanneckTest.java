3e605a76190f8704f80d789dd2968d29a6c5038d
lange <lange@SURFACE-LANGE>
2016-11-09 22:16:04 +0100

    ADS Uebung 2 Aufgabe 5

--- GDI 1IB/src/freierTest/JanneckTest.java
+++ GDI 1IB/src/freierTest/JanneckTest.java
@@ -2,65 +2,79 @@ package freierTest;
 
 import static gdi.MakeItSimple.*;
 
+import gdi.MakeItSimple.GDIException;
 
 public class JanneckTest {
 
 	
-	static boolean aenderung(int[] kaenderung) {
-		int i=0;
-		int z=0;
-		while (i< kaenderung.length){
-			if(kaenderung[i] == 1){
-				z+=1;
-			}else if(kaenderung[i] == 0){
-				
-			}i++;
-		}
-		if(z == kaenderung.length){
-			return true;
-		}		
-		return false;
-		
-	}
-	
-	public static void main(String[] args) {
-		
-	
-		int x;
-		int y;
-		int i = 0;
-		print("Kartenanzahl: ");
-		int kanzahl = readInt();
-		int[] kaenderung = new int[kanzahl];
-		int[] karten = new int[kanzahl];
-		int i1=0;
-		
-		while(i1<kanzahl){
-			print("stelle " + (i1+1) );
-			karten[i1] = readInt();
-			i1++;
+	static int a1(int n){
+		int sum = 0;
+
+		if (n < 0){
+			throw new GDIException("Ungültiger Parameter! Es wird eine Zahl größer/gleich 0 erwartet!");
 		}
 		
-		while (aenderung(kaenderung) == true){
-			
-			while(i<karten.length){
-				if(karten[i]<karten[i+1]){
-					kaenderung[i] = 1;
-				}else if(karten[i]>karten[i+1]){
-					x=i;
-					y=i+1;
-					karten[i]=y;
-					karten[i+1]=x;
-					kaenderung[i] = 0;
+		if (n == 0 || n == 1) {
+			return 1;
+		} else {
+			int[] zahlenFolge = new int[n];
+
+			int i = 0;
+			if (n >= 1)
+				zahlenFolge[i] = 1;
+			i++;
+			if (n >= 2)
+				zahlenFolge[i] = 1;
+			i++;
+			if (n >= 3)
+				while (i < n) {
+					zahlenFolge[i] = zahlenFolge[i - 2] + zahlenFolge[i - 1];
+					i++;
 				}
-				i++;
-				
-			}
-			i=0;
+
+			sum = zahlenFolge[n - 1] + zahlenFolge[n - 2];
+			return sum;
 		}
-		int i2 = 0;;
-		while( i2<karten.length){
-			print(karten[i2]);
-			i2++;
+	}
+
+	static int a2(int n) {
+		int a = 1;
+		int b = 1;
+		int zwischenSpeicher = 0;
+		
+		if (n < 0){
+			throw new GDIException("Ungültiger Parameter! Es wird eine Zahl größer/gleich 0 erwartet!");
 		}
-	}}
+		
+		while (n!=0){
+			n = n-1;
+			zwischenSpeicher = b;
+			b = a+b;
+			a = zwischenSpeicher;
+		}
+		return a;
+	}
+
+	public static void main(String[] args) {
+
+		println("Welchen Stelle der Fibonacci-Folge wollen sie berechnen?");
+		int n = readInt();
+
+		println("Wollen sie nach A1 oder nach A2 verfahren?");
+		println("Geben sie 1 für A1 ein und 2 für A2");
+		int wahl = readInt();
+		int a;
+
+		if (wahl == 1) {
+			a=a1(n);
+		} else if (wahl == 2) {
+			a=a2(n);
+
+		} else {
+			println("Fehlerhafte eingabe");
+			return;
+		}
+		println("Die Gesuchte Zahl ist: " + a);
+
+	}
+}
