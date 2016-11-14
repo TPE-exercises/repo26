fe459952a30393361948bb8b2202cc5b27ed2a37
lange <lange@141.19.159.81>
2016-11-14 16:42:20 +0100

    Programm ist Fertig!!!

--- GDI 1IB/src/ads/searchFunction.java
+++ GDI 1IB/src/ads/searchFunction.java
@@ -6,35 +6,38 @@ public class searchFunction {
 	
 	public static void main(String[] args) {
 	
-		int choise = 0;
-		int result = 0;
-		int number = 0;
+		int result1 = 0;
+		int result2 = 0;
+		int result3 = 0;
+		int result4 = 0;
+		int searchNumber = 0;
 		int[] numberSequence;
 		
-		println("Wie lang ist die zu sortierende Folge?");
-		int length = readInt();
-		numberSequence = createNumberSequence(length);
-		
-		println("Mit welcher Methode soll gesucht werden?");
-		choise = readInt();
+		/*println("Wie lang ist die zu sortierende Folge?");
+		int length = readInt();*/ int length = 10;
+		numberSequence = createDNumberSequence(1);
+	
 		println("Welche Zahl soll gesucht werden?");
+		searchNumber = readInt();
 				
-		if (choise == 1){
-			result = linearSearchA(numberSequence, number);
-		}
-		else if(choise == 2){
-			result = linearSearchB(numberSequence, number);
-		}
-		else if (choise == 3){
-			result = BinarySearchRecursive(numberSequence, number);
-		}
-		else if (choise == 4){
-			result = BinarySearchIterative(numberSequence, number);
-		}
-		else 
-			println("Falsche eingabe");
+			result1 = linearSearchA(numberSequence, searchNumber);
+			result2 = linearSearchB(numberSequence, searchNumber);
+			result3 = BinarySearchRecursive(numberSequence, searchNumber, 0, numberSequence.length-1);
+			result4 = BinarySearchIterative(numberSequence, searchNumber);
+			
+			println("Array________________________");
+			for (int i = 0; i < length; i++) {
+				print(i + "  ");
+			}println();
+			for (int i = 0; i < length; i++) {
+				print(numberSequence[i] + " ");
+			}println();
+			println("_____________________________");
 
-		println(result);
+		println("Index der Linearen Suche 1: " + result1);
+		println("Index der Linearen Suche 2: " + result2);
+		println("Index der Binären Suche rekusiv: " + result3);
+		println("Index der Binären Suche iterativ: " + result4);
 	}
 	
 	static int[] createNumberSequence (int length){
@@ -49,28 +52,79 @@ public class searchFunction {
 		return numberSequence;
 	}
 	
+	static int[] createRandomNumberSequence (int length){
+		int[] numberSequence = new int[length];
+		
+		for(int i = 0; i < length; i++){
+			int random = (int) Math.floor(Math.random()*99);
+			numberSequence[i] = random;
+		}
+		return numberSequence;
+
+	}
+	static int[] createDNumberSequence (int length){
+		int[] numberSequence = {5,10,11,13,14,20,30,32,35,40};
+		return numberSequence;
+	}
 	
-	static int linearSearchA (int[] numberSequence, int number){
-		
-		
-		
-		return -1;
+	//Return last searchNumber in array
+	static int linearSearchA (int[] numberSequence, int searchNumber){
+		int x = -1;
+		for(int i = 0; i < numberSequence.length; i++){
+			if(numberSequence[i] == searchNumber){
+				x = i;
+			}
+		}
+		return x;
 	}
-	static int linearSearchB (int[] numberSequence, int number){
-		
-		
-		
-		return -1;
+	
+	//Return first searchNumber in array
+	static int linearSearchB (int[] numberSequence, int searchNumber){
+		boolean found = false; 
+		int x = -1;
+		for(int i=0; i < numberSequence.length && !found; i++){
+			if(numberSequence[i] == searchNumber){
+				found = true;
+				x = i;
+				return x;
+			}
+		}
+		return x;
 	}
-	static int BinarySearchRecursive (int[] numberSequence, int number){
+	
+	static int BinarySearchRecursive (int[] numberSequence, int searchNumber, int u, int o){
+
 		
-		
-		
-		return -1;
+		  if(u>o)
+			 return -1;
+		  
+		  int m = (u+o)/2;
+		  
+		 if (numberSequence[m] == searchNumber)
+			 return m;	 
+			
+		 else if(searchNumber < numberSequence[m])
+			 return BinarySearchRecursive(numberSequence, searchNumber, u, m-1);
+			 
+		 else //(searchNumber > numberSequence[m])
+			 return BinarySearchRecursive(numberSequence, searchNumber, m+1, o);
+			
 	}
-	static int BinarySearchIterative (int[] numberSequence, int number){
-		
-		
+	
+	static int BinarySearchIterative (int[] numberSequence, int searchNumber){
+		 int u = 0;
+		 int o = numberSequence.length-1;
+		 
+		 while(u<=o){
+			 int m = (u+o)/2;
+			 if (numberSequence[m] == searchNumber)
+				 return m;
+			 
+			 else if(searchNumber < numberSequence[m])
+				 o = m-1;
+			 else
+				 u = m+1;
+		 } 
 		
 		return -1;
 	}
