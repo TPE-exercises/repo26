52b13d5e8e885c35bc1a3a74a72b382f2a1b1e14
lange <lange@141.19.152.25>
2016-11-16 17:08:53 +0100

    Debug hinzugefügt, sowie korrektes Feld-2.txt

--- GDI 1IB/src/de/hsMannheim/informatik/gdi/uebung06/EightQueens.java
+++ GDI 1IB/src/de/hsMannheim/informatik/gdi/uebung06/EightQueens.java
@@ -7,16 +7,32 @@ public class EightQueens {
 		int[][] field = new int [8][8];
 		boolean solution;
 		
+		boolean debug = true;
 		
-		field = readFieldFromFile("Field-1.txt");
-		solution = isValidSolution(field);
+		println("Gültig 1 oder ungültig 2");
+		int wahl = readInt();
 		
-		//Gebe Spielbrett aus
-		for(int n=0; n<8;n++){
-			println();
-			for(int i=0; i<8;i++)
-				print(field[n][i] + " ");
+		switch (wahl) {
+		case 1:
+			field = readFieldFromFile("Field-2.txt");
+			break;
+		case 2:
+			field = readFieldFromFile("Field-1.txt");
+			break;
+		default:
+			println("Ungültige Eingabe");
 		}
+		solution = isValidSolution(field, debug);
+		
+		if(debug){
+		//Gebe Spielbrett aus
+			for(int n=0; n<8;n++){
+				println();
+				for(int i=0; i<8;i++){
+					print(field[n][i] + " ");		
+				}
+			}
+		}println();
 		println(solution);
 	}
 	
@@ -36,44 +52,108 @@ public class EightQueens {
 		return field;
 	}
 	
-	static boolean isValidSolution(int[][] field){
+	static boolean isValidSolution(int[][] field, boolean debug){
 		boolean d, v, h;
 		int damenZähler = 0;
 		
 		for(int zeile=0; zeile<8;zeile++){
 			for(int spalte=0; spalte<8;spalte++){
 				if(field[zeile][spalte] == 1){
-					d = isValidSolutionDiagonal(field, zeile, spalte);
-					v = isValidSolutionVertikal(field, spalte);
-					h = isValidSolutionHorizontal(field, zeile);
+					d = isValidSolutionDiagonal(field, zeile, spalte, debug);
+					v = isValidSolutionVertikal(field, spalte, debug);
+					h = isValidSolutionHorizontal(field, zeile, debug);
 					damenZähler++;
-			//println("h ist " + zeile + h);
+			
+					if(debug){
+						println("Testzeile " + zeile);
+						println("Vertikal ist " + v);
+						println("Horizontal ist " + h);
+						println("Diagonal ist " + d);
+						println("------------------------------------------------");
+					}
 					if(!d || !v || !h){
 						return false;
 					}
 				}
 			}	
 		}
-		
+		if(debug)	
+			println("Es gibt " + damenZähler + " Damen");
 		if(damenZähler == 8)
 			return true;
 		else 
 			return false;
 	}
-	static boolean isValidSolutionDiagonal(int[][] field, int zeile, int spalte){
+	static boolean isValidSolutionDiagonal(int[][] field, int zeile, int spalte, boolean debug){
+
+		int zählerZeile = zeile;
+		int zählerSpalte = spalte;
+		int damenZähler = 0;
 		
+		while(zählerZeile < 8 && zählerSpalte < 8){
+			if(field[zählerZeile][zählerSpalte] == 1){
+				damenZähler++;
+				if(debug && damenZähler > 1){
+					println("------------------------------------------------");
+					println("Dame auf " + zeile + ", " + spalte + " bedroht");
+					println("Dame " + (damenZähler-1) + " diagonal auf " + zählerZeile + ", "+ zählerSpalte);
+					println("------------------------------------------------");
+				}
+			}
+			zählerSpalte++;
+			zählerZeile++;
+		}
 		
-		return true;
+		zählerZeile = zeile;
+		zählerSpalte = spalte;
+		
+		while(zählerZeile < 8 && zählerSpalte >= 0){
+			if(field[zählerZeile][zählerSpalte] == 1){
+				damenZähler++;
+				if(debug && damenZähler >2){
+					println("------------------------------------------------");
+					println("Dame auf " + zeile + ", " + spalte + " bedroht");
+					println("Dame " + (damenZähler-1) + " diagonal auf " + zählerZeile + ", "+ zählerSpalte);
+					println("------------------------------------------------");
+				}
+			}
+			zählerSpalte--;
+			zählerZeile++;
+		}
+		
+		if(damenZähler==2)
+			return true;
+		else
+			return false;
+		
 	}
-	static boolean isValidSolutionVertikal(int[][] field, int spalte){
-		
-		return true;
+	static boolean isValidSolutionVertikal(int[][] field, int spalte, boolean debug){
+		int damenZähler = 0;
+		for(int i=0; i<8;i++){
+			if(field[i][spalte] == 1){
+				damenZähler++;	
+			}
+			if(debug && damenZähler > 2){
+				println("------------------------------------------------");
+				println("Damen auf Spalte " + spalte + " bedrohen sich.");
+				println("------------------------------------------------");
+			}
+		}
+		if(damenZähler==1)
+			return true;
+		else
+			return false;
 	}
-	static boolean isValidSolutionHorizontal(int[][] field, int zeile){
+	static boolean isValidSolutionHorizontal(int[][] field, int zeile, boolean debug){
 		int damenZähler = 0;
 		for(int i=0; i<8;i++){
 			if(field[zeile][i] == 1){
 				damenZähler++;
+			}
+			if(debug && damenZähler > 1){
+				println("------------------------------------------------");
+				println("Damen auf Zeile " + zeile + " bedrohen sich.");
+				println("------------------------------------------------");
 			}
 		}
 		if(damenZähler==1)
