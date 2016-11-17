330582fe446c9a4f39235ddd8eaa40b910574246
lange <lange@141.19.152.174>
2016-11-17 10:46:44 +0100

    keine lösung gefunden programm falsch neuer ansatz notwendig

--- GDI 1IB/src/de/hsMannheim/informatik/gdi/uebung06/FindEightQueensSolution.java
+++ GDI 1IB/src/de/hsMannheim/informatik/gdi/uebung06/FindEightQueensSolution.java
@@ -5,8 +5,67 @@ import static gdi.MakeItSimple.*;
 public class FindEightQueensSolution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[][] field = new int [8][8];
+		boolean debug = false;
+		int damenZähler = 0;
+		
+		for(int zeile=0; zeile<8; zeile++){
+			int zählerinZeile = 0;
+			int newspalte = 0;
+			
+			for(int spalte=0; spalte<8; spalte++){
+				//println(zeile + ", " + spalte);
+				
+				newspalte = spalte;
+				if(field[zeile][spalte] == 0)
+					field[zeile][spalte] = 1;
+				
+				zählerinZeile++;
+				
+				boolean d = EightQueens.isValidSolutionDiagonal(field, zeile, spalte, debug);
+				boolean du = EightQueens.isValidSolutionDiagonalUp(field, zeile, spalte, debug);
+				boolean v = EightQueens.isValidSolutionVertikal(field, spalte, debug);
+				boolean h = EightQueens.isValidSolutionHorizontal(field, zeile, debug);
+				
+				if(!d || !du || !v || !h){
+					field[zeile][spalte] = 0;
+					zählerinZeile--;
+				}
+							println("Zeile: " + zeile + " Spalte: " + spalte + " Zähler: " + zählerinZeile);
+					
+				damenZähler += zählerinZeile;
+				
+			}
+				println("__--__--__--__--__--__--__--__--__--__--__");
+				println(zählerinZeile);
+				if(zählerinZeile == 0){
+					for(int i=0; i<8; i++){
+						
+						//field[zeile][i] = 0;
+					}
+					for(int i=0; i<8; i++){
+						if(field[zeile-1][i] == 1)
+							field[zeile-1][i] = 4;
+					}
+					for(int n=0; n<8;n++){
+						println();
+						for(int i=0; i<8;i++){
+							print(field[n][i] + " ");		
+						}
+					}
+					zeile = zeile-2;
+				}	
+		}
+	
+		
+		for(int n=0; n<8;n++){
+			println();
+			for(int i=0; i<8;i++){
+				print(field[n][i] + " ");		
+			}
+		}
+	
 	}
 
 }
