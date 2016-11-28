8f4fe28f7d06cdb452607a12e2d4b688a095b4db
lange <lange@141.19.156.25>
2016-11-28 18:19:53 +0100

    merge übertragen

--- GDI 1IB/src/de/hsMannheim/informatik/ads/uebung04/MergeSort.java
+++ GDI 1IB/src/de/hsMannheim/informatik/ads/uebung04/MergeSort.java
@@ -4,134 +4,165 @@ import static gdi.MakeItSimple.*;
 
 public class MergeSort {
 
-	static int[] zuSortierendesArray;
-	static int[] sortiertesArray = new int[_Main.length];
+	public static int[] eingabeArray = { 10, 5, 15, 3, 6, 9, 12, 16, };
+	static final int length = 8;
+	static int tl = 1;// aktuelle Lauflänge | Blocklänge
+	static int runs = 1;
+	static int runGeneral = 2;
 
-	/*
-	public static void main(String[] args) {
-		_Main.main(args);
+	static void trenner() {
+		print("-------------");
 	}
-	*/
+	static int[] createRandomNumberSequence(int length, int n) {
+		int[] numberSequence = new int[length];
 
-	static int[] direkterMergeSort(int[] numberSequence, int anzahlSplits) {
-		zuSortierendesArray = einlesenDesEingabeArrays();
-		println("zu sortierendes Array:");
-		print(zuSortierendesArray[0]);
-		for (int i = 1; i < _Main.length; i++) {
-			print(", " + zuSortierendesArray[i]);
+		for (int i = 0; i < length; i++) {
+			int random = (int) Math.floor(Math.random() * n);
+			numberSequence[i] = random;
 		}
-		println();
-		for (int k = 1; k <= 3; k++) {
-			anzahlSplits /= 2;
-			split(k, anzahlSplits);
-			merge(k, anzahlSplits);
-			zuSortierendesArray = einlesenDesEingabeArrays();
-			print(zuSortierendesArray[0]);
-			for (int i = 1; i < _Main.length; i++) {
-				print(", " + zuSortierendesArray[i]);
-			}
-		}
-		zuSortierendesArray = einlesenDesEingabeArrays();
-		print(zuSortierendesArray[0]);
-		for (int i = 1; i < _Main.length; i++) {
-			print(", " + zuSortierendesArray[i]);
-		}
-		println();
-		// split(2, anzahlZahlen / 2);
-		// merge(2, anzahlZahlen / 2);
-		return zuSortierendesArray;
+		return numberSequence;
+
 	}
 
-	/*
-	 * static void arrayInDateiSchreiben(int[] numberSequence) { Object file =
-	 * openOutputFile("EingabeArray.txt"); for (int i = 0; i < _Main.length;
-	 * i++) { print(file, _Main.eingabeArray[i] + " "); } closeOutputFile(file);
-	 * }
-	 */
+	public static void erstelleHauptband() {
+		Object hauptband = openOutputFile("Hauptband.txt");
+		for (int i = 0; i < eingabeArray.length; i++)
+			print(hauptband, eingabeArray[i] + "\n");
+		closeOutputFile(hauptband);
 
-	static int[] einlesenDesEingabeArrays() {
-		int[] array = new int[_Main.length];
-		if (isFilePresent("Hauptband.txt")) {
-			Object file = openInputFile("Hauptband.txt");
-			for (int i = 0; i < _Main.length; i++) {
-				array[i] = readInt(file);
-			}
-			closeInputFile(file);
-		} else
-			throw new GDIException("Die Datei \"Hauptband.txt\" konnte nicht gefunden werden.");
-		return array;
 	}
 
-	static void merge(int durchgang, int häufigkeit) {
-		Object Band1 = openInputFile("Band1.txt");
-		Object Band2 = openInputFile("Band2.txt");
-		Object einleseBand = openOutputFile("Hauptband.txt");
-		println("Merge:");
+	public static void merge() {
 
-		for (int z = 1; z <= häufigkeit; z++) {
-			int splitTo1 = readInt(Band1);
-			int splitTo2 = readInt(Band2);
+		int blockPosition = 0;
 
-			for (int i = 1; i <= durchgang; i++) {
-
-				if (splitTo1 <= splitTo2) {
-					print(einleseBand, splitTo1 + " ");
-					if (!(i+1>=durchgang)){
-							splitTo1 = readInt(Band1);
-					}
-					else{
-					}
-					
-				} else {
-					print(einleseBand, splitTo2 + " ");
-				}
-			}
-			println(einleseBand);
-
-		}
-		closeInputFile(Band1);
-		closeInputFile(Band2);
-		closeOutputFile(einleseBand);
-	}
-	static int[] leseDateiZuArray (int durchgänge, String filename){
-		int[] array = new int[durchgänge];
-
-		if (isFilePresent(filename)) {
-			Object file = openInputFile(filename);
-			for (int i = 0; i < _Main.length; i++) {
-				array[i] = readInt(file);
-			}
-			closeInputFile(file);
-		} else
-			throw new GDIException("Die Datei " + filename + " konnte nicht gefunden werden.");
-		return array;
-		
-	}
-
-	static void split(int durchgang, int häufigkeit) {
-		Object einleseBand = openInputFile("Hauptband.txt");
-		Object Band1 = openOutputFile("Band1.txt");
-		Object Band2 = openOutputFile("Band2.txt");
-		println("Split:");
-		for (int i = 0; i < häufigkeit; i++) {
-			int splitTo1 = readInt(einleseBand);
-
-			for (int j = durchgang; j > 0; j--) {
-				print(Band1, splitTo1 + " ");
-				print("Band1: " + splitTo1 + "; ");
-				println(Band1);
-			}
-
-			for (int j = durchgang; j > 0; j--) {
-				int splitTo2 = readInt(einleseBand);
-				print(Band2, splitTo2 + " ");
-				print("Band2: " + splitTo2);
-				println(Band2);
-			}
+		// läuft bis sortiert
+		while (tl <= length) {
+			/**
+			 * Split Start
+			 */
+			trenner();
+			print("Split Start");
+			trenner();
 			println();
+			Object hauptband = openInputFile("Hauptband.txt");
+			Object band1 = openOutputFile("Band1.txt");
+			Object band2 = openOutputFile("Band2.txt");
+
+			for (int j = 0; j < length; j++) {
+
+				if (blockPosition < tl) {
+					int hauptNumber = readInt(hauptband);
+					print(band1, hauptNumber + "\n");
+					blockPosition++;
+				} else {
+					int hauptNumber = readInt(hauptband);
+					print(band2, hauptNumber + "\n");
+					blockPosition++;
+				}
+				if (blockPosition >= tl * 2)
+					blockPosition = 0;
+			}
+
+			closeInputFile(hauptband);
+			closeOutputFile(band1);
+			closeOutputFile(band2);
+
+			printBand("band1.txt", 4);
+			printBand("band2.txt", 4);
+
+			trenner();
+			print("Split Ende");
+			trenner();
+			println();
+			/**
+			 * Split Ende
+			 * 
+			 * Merge Start
+			 */
+
+			band1 = openInputFile("band1.txt");
+			band2 = openInputFile("band2.txt");
+			hauptband = openOutputFile("hauptband.txt");
+			runs = 1;
+			// läuft bis zum ende der datei (alle blöcke)
+			for (int i = 1; i <= (length / runGeneral); i++) {
+
+				int indexBand1 = -1;
+				int indexBand2 = -1;
+
+				// läuft bis zum ende von tl
+				while (indexBand1 < tl && indexBand2 < tl) {
+					trenner();
+					print("Merge Runde " + runs++ + " Start");
+					trenner();
+					println();
+
+					int splitTo1 = readInt(band1);
+					indexBand1++;
+					int splitTo2 = readInt(band2);
+					indexBand2++;
+
+					while (indexBand1 < tl && indexBand2 < tl) {
+						if (splitTo1 < splitTo2) {
+							print(hauptband, splitTo1 + "\n");
+							indexBand1++;
+							if (indexBand1 < tl)
+								splitTo1 = readInt(band1);
+						} else {
+							print(hauptband, splitTo2 + "\n");
+							indexBand2++;
+							if (indexBand2 < tl)
+								splitTo2 = readInt(band2);
+						}
+					}
+
+					if (indexBand1 >= tl) {
+						while (indexBand2 < tl) {
+							print(hauptband, splitTo2 + "\n");
+							indexBand2++;
+							if (indexBand2 < tl)
+								splitTo2 = readInt(band2);
+						}
+					} else if (indexBand2 >= tl) {
+						while (indexBand1 < tl) {
+							print(hauptband, splitTo1 + "\n");
+							indexBand1++;
+							if (indexBand1 < tl)
+								splitTo1 = readInt(band1);
+						}
+					}
+
+				}
+				// Füge den verbleibenden nichtleeren Rest des aktuellen Laufs
+				// von F1 oder F2 an F an
+
+				printBand("hauptband.txt", (runs * 2) - 2);
+			}
+			tl = tl * 2;
+			runGeneral = runGeneral * 2;
+			closeInputFile(band1);
+			closeInputFile(band2);
+			closeOutputFile(hauptband);
+
 		}
-		closeInputFile(einleseBand);
-		closeOutputFile(Band1);
-		closeOutputFile(Band2);
+	}
+
+	private static void printBand(String file, int dateiLänge) {
+		boolean endOfFile = false;
+
+		Object band = openInputFile(file);
+		print(file + ": ");
+		for (int i = 0; i < dateiLänge; i++) {
+			print(readInt(band) + ", ");
+		}
+		closeInputFile(band);
+		println();
+	}
+
+	public static void main(String[] args) {
+		eingabeArray = createRandomNumberSequence(8, 50); // 2, 4, 8, 16, 32, 64, 128
+		erstelleHauptband();
+		MergeSort.merge();
 	}
 }
