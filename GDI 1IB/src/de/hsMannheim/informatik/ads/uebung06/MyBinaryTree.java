c3cf99a1cdf518c2f497071624d7de5ab128d6da
lange <lange@SURFACE-LANGE.fritz.box>
2017-01-10 18:35:50 +0100

    Weiterarbeit an MyBinaryTree

--- GDI 1IB/src/de/hsMannheim/informatik/ads/uebung06/MyBinaryTree.java
+++ GDI 1IB/src/de/hsMannheim/informatik/ads/uebung06/MyBinaryTree.java
@@ -1,96 +1,146 @@
 package de.hsMannheim.informatik.ads.uebung06;
+
 import static gdi.MakeItSimple.*;
+
 public class MyBinaryTree implements BinaryTree {
-public static TreeNode root;
-private static MyBinaryTree tree = new MyBinaryTree();
-	
-	MyBinaryTree(){
+	public static TreeNode root;
+	private static MyBinaryTree tree = new MyBinaryTree();
+	private static MyBinaryTree cloneTree = new MyBinaryTree();
+
+	MyBinaryTree() {
 		root = null;
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 
 		boolean done = false;
-		while(!done){
-		println("Was wollen Sie ausführen?");
+		while (!done) {
+			println("Was wollen Sie ausführen?");
 
-		println("1. insert (val)");
-		println("2. insert (file)");
-		println("3. contains");
-		println("4. size");
-		println("5. hight");
-		println("6. getMax");
-		println("7. getMin");
-		println("8. remove");
-		println("9. isEmpty");
-		println("10. addAll");
-		println("11. printInorder");
-		println("12. printPostorder");
-		println("13. printPreorder");
-		println("14. printLevelorder");
-		println("15. clone");
-		println("0. beenden");
+			println("1. insert (val)");
+			println("2. insert (file)");
+			println("3. contains");
+			println("4. size");
+			println("5. hight");
+			println("6. getMax");
+			println("7. getMin");
+			println("8. remove");
+			println("9. isEmpty");
+			println("10. addAll");
+			println("11. printInorder");
+			println("12. printPostorder");
+			println("13. printPreorder");
+			println("14. printLevelorder");
+			println("15. clone");
+			println("0. beenden");
 
-		int selection = readInt();
-		readLine();
-		separator();
-		switch (selection) {
-		case (1):
-			
-		
-			print("Geben Sie den Wert ein, den sie einfügen wollen: ");
-			int val = readInt();
+			int selection = readInt();
 			readLine();
-			boolean succses = false;
-					
-			succses = tree.insert(val);
-			
-			if(succses)
-				println("Der Wert " + val + " wurde erfolgreich eingefügt.");
-			else
-				println("Der Wert " + val + " wurde nicht eingefügt.");
-			
-			break;
-		case (2):
-			break;
-		case (3):
-			break;
-		case (4):
-			break;
-		case (5):
-			break;
-		case (6):
-			break;
-		case (7):
-			break;
-		case (8):
-			break;
-		case (9):
-			break;
-		case (10):
-			break;
-		case (11):
-			break;
-		case (12):
-			break;
-		case (13):
-			break;
-		case (14):
-			break;
-		case (15):
-			break;
-		case (0):
-			done=true;
-		}
+			separator();
+			switch (selection) {
+			case (1): // Insert (val)
+
+				print("Geben Sie den Wert ein, den sie einfügen wollen: ");
+				int val = readInt();
+				readLine();
+				boolean succses = false;
+
+				succses = tree.insert(val);
+
+				if (succses)
+					println("Der Wert " + val + " wurde erfolgreich eingefügt.");
+				else
+					println("Der Wert " + val + " wurde nicht eingefügt.");
+				separator();
+
+				break;
+			case (2): // insert (file)
+				// _______________________________________________________________________<--
+				separator();
+				break;
+			case (3): // contains
+				print("Geben Sie den Wert ein, den Sie prüfen wollen: ");
+				val = readInt();
+				readLine();
+
+				if (tree.contains(val))
+					println("Der Wert " + val + " ist im Baum");
+				else
+					println("Der Wert " + val + " ist nicht im Baum");
+				separator();
+
+				break;
+			case (4): // size
+				// _______________________________________________________________________<--
+				separator();
+				break;
+			case (5): // hight
+				// _______________________________________________________________________<--
+				separator();
+				break;
+			case (6): // getMax
+				println("Der höchste Wert im Baum ist: " + tree.getMax());
+				separator();
+
+				break;
+			case (7): // getMin
+				println("Der kleinste Wert im Baum ist: " + tree.getMin());
+				separator();
+				break;
+			case (8): // remove (val)
+				print("Geben Sie den Wert ein, den Sie entfernen wollen: ");
+				val = readInt();
+				readLine();
+
+				if (tree.remove(val))
+					println("Der Wert " + val + " wurde entfernt");
+				else
+					println("Der Wert " + val + " ist nicht im Baum");
+				separator();
+				break;
+			case (9): // isEmpty
+				if (tree.isEmpty())
+					println("Der Baum ist leer.");
+				else
+					println("Der Baum ist nicht leer");
+				separator();
+				break;
+			case (10): // addAll
+				// _______________________________________________________________________<--
+				separator();
+				break;
+			case (11): // printInorder
+				// _______________________________________________________________________<--
+				separator();
+				break;
+			case (12): // printPostorder
+				// _______________________________________________________________________<--
+				separator();
+				break;
+			case (13): // printPreorder
+				// _______________________________________________________________________<--
+				separator();
+				break;
+			case (14): // printLevelorder
+				// _______________________________________________________________________<--
+				separator();
+				break;
+			case (15): // clone (deep)
+				tree.clone();
+				separator();
+				break;
+			case (0):
+				done = true;
+			}
 		}
 
 	}
-	
-	private static void separator(){
+
+	private static void separator() {
 		println("-------------------------------------");
 	}
-	
+
 	/**
 	 * Fügt val in den Baum ein.
 	 */
@@ -98,26 +148,26 @@ private static MyBinaryTree tree = new MyBinaryTree();
 
 		TreeNode parent = null;
 		TreeNode child = root;
-		
-		while(child != null){
+
+		while (child != null) {
 			parent = child;
-			if(child.getValue() == 0)
+			if (child.getValue() == val)
 				return false;
-			else if(child.getValue() < 0)
+			else if (child.getValue() > val)
 				child = child.getLeft();
 			else
 				child = child.getRight();
 		}
-		
-		if(parent == null)
-		    root = new TreeNode(val);
-		else if(parent.getValue() < 0)
+
+		if (parent == null)
+			root = new TreeNode(val);
+		else if (parent.getValue() > val)
 			parent.setLeft(new TreeNode(val));
 		else
 			parent.setRight(new TreeNode(val));
-		
+
 		return true;
-		
+
 	}
 
 	/**
@@ -132,7 +182,20 @@ private static MyBinaryTree tree = new MyBinaryTree();
 	 * Testet, ob val im Baum vorhanden ist.
 	 */
 	public boolean contains(int val) {
-		// TODO Auto-generated method stub
+
+		TreeNode parent = null;
+		TreeNode child = root;
+
+		while (child != null) {
+			parent = child;
+			if (child.getValue() == val)
+				return true;
+			else if (child.getValue() > val)
+				child = child.getLeft();
+			else
+				child = child.getRight();
+		}
+
 		return false;
 	}
 
@@ -156,42 +219,112 @@ private static MyBinaryTree tree = new MyBinaryTree();
 	 * Liefert das größte Element im Baum.
 	 */
 	public int getMax() {
-		// TODO Auto-generated method stub
-		return 0;
+		TreeNode child = root;
+		int valMax = 0;
+
+		while (child != null) {
+			valMax = child.getValue();
+			child = child.getRight();
+		}
+
+		return valMax;
 	}
 
 	/**
 	 * Liefert das kleinste Element im Baum.
 	 */
 	public int getMin() {
-		// TODO Auto-generated method stub
-		return 0;
+		TreeNode child = root;
+		int valMin = 0;
+
+		while (child != null) {
+			valMin = child.getValue();
+			child = child.getLeft();
+		}
+
+		return valMin;
 	}
 
 	/**
 	 * Entfernt val aus dem Baum.
 	 */
 	public boolean remove(int val) {
-		// TODO Auto-generated method stub
-		return false;
+		TreeNode parent = null;
+		TreeNode child = root;
+		TreeNode help = null;
+		TreeNode helpParent = null;
+		boolean walkLeft = false;
+		boolean noHelpParen = false;
+
+		if (tree.contains(val)) {
+			while (child != null) {
+
+				if (child.getValue() == val) {
+					// Suche einzufügenden Node
+					// ERR keine getLeft
+					if (child.getLeft() != null) {
+						help = child.getLeft();
+						while (help.getRight() != null) {
+							helpParent = help;
+							help = help.getRight();
+							noHelpParen = true;
+						}
+						if (!noHelpParen) {
+							child.setLeft(null);
+						} else {
+							helpParent.setRight(null);
+						}
+
+						if (child.getLeft() != null)
+							help.setLeft(child.getLeft());
+						if (child.getRight() != null)
+							help.setRight(child.getRight());
+						child = help;
+						if (walkLeft)
+							parent.setLeft(help);
+						else
+							parent.setRight(help);
+
+						return true;
+					}else{
+						parent.setRight(child.getRight());
+					}
+				} else if (child.getValue() > val) {
+					parent = child;
+					child = child.getLeft();
+					walkLeft = true;
+				} else {
+					parent = child;
+					child = child.getRight();
+					walkLeft = false;
+				}
+			}
+			return false;
+
+		} else
+			return false;
 	}
 
 	/**
 	 * true genau dann, wenn der Baum leer ist.
 	 */
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		TreeNode child = root;
+		if (child == null)
+			return true;
+		else
+			return false;
 	}
 
 	/**
-	 * Fügt alle Elemente des übergebenen Baums (otherTree) in den aktuellen Baum ein.
+	 * Fügt alle Elemente des übergebenen Baums (otherTree) in den aktuellen
+	 * Baum ein.
 	 */
-	public boolean addAll(BinaryTree otherTree){
+	public boolean addAll(BinaryTree otherTree) {
 		return false;
-		
+
 	}
-	
+
 	/**
 	 * Gibt Baum in Inorder aus.
 	 */
@@ -228,18 +361,48 @@ private static MyBinaryTree tree = new MyBinaryTree();
 	 * Erzeugt eine tiefe Kopie des Baums.
 	 */
 	public BinaryTree clone() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		
+		TreeNode node = root;
+		
+		if(node == null)
+			return cloneTree;
+		
+		TreeNode
+		
+		node.setValue();
+		cloneTree.insert(node.getValue());
+		
+		if(node.getLeft() != null)
+			cloneDeep(node.getLeft());
+		if(node.getRight() != null)
+			cloneDeep(node.getRight());
+
+		return cloneTree;
 	}
 	
-	/**
-	 * Erzeugt einen leeren neuen Baum
-	 * @return
-	 */
-	BinaryTree empty (){
-		return null;
+	public void cloneDeep(TreeNode node){
+		
+		cloneTree.insert(node.getValue());
+		
+		if(node.getLeft() != null)
+			cloneDeep(node.getLeft());
+		if(node.getRight() != null)
+			cloneDeep(node.getRight());
 		
 	}
 
+	/**
+	 * Erzeugt einen leeren neuen Baum
+	 * 
+	 * @return
+	 */
+	BinaryTree empty() {
+
+		MyBinaryTree newTree = new MyBinaryTree();
+
+		return newTree;
+
+	}
 
 }
--- GDI 1IB/src/freierTest/JanneckTest.java
+++ GDI 1IB/src/freierTest/JanneckTest.java
@@ -2,81 +2,11 @@ package freierTest;
 
 import static gdi.MakeItSimple.*;
 
-import gdi.MakeItSimple.GDIException;
 
 public class JanneckTest {
-//Fibunacchi
 	
-	static int a1(int n){
-		int sum = 0;
-
-		if (n < 0){
-			throw new GDIException("Ungültiger Parameter! "
-					+ "Es wird eine Zahl größer/gleich 0 erwartet!");
-		}
-		if (n == 0 || n == 1) {
-			return 1;
-		} else {
-			int[] zahlenFolge = new int[n];
-
-			int i = 0;
-			if (n >= 1)
-				zahlenFolge[i] = 1;
-			i++;
-			if (n >= 2)
-				zahlenFolge[i] = 1;
-			i++;
-			if (n >= 3)
-				while (i < n) {
-					zahlenFolge[i] = zahlenFolge[i - 2] + 
-							zahlenFolge[i - 1];
-					i++;
-				}
-			sum = zahlenFolge[n - 1] + zahlenFolge[n - 2];
-			return sum;
-		}
-	}
-
-	static int a2(int n) {
-		int a = 1;
-		int b = 1;
-		int zwischenSpeicher = 0;
-		
-		if (n < 0){
-			throw new GDIException("Ungültiger Parameter! "
-					+ "Es wird eine Zahl größer/gleich 0 erwartet!");
-		}
-		
-		while (n!=0){
-			n = n-1;
-			zwischenSpeicher = b;
-			b = a+b;
-			a = zwischenSpeicher;
-		}
-		return a;
-	}
-
+	
 	public static void main(String[] args) {
-
-		println("Welchen Stelle der Fibonacci-Folge "
-				+ "wollen sie berechnen?");
-		int n = readInt();
-
-		println("Wollen sie nach A1 oder nach A2 verfahren?");
-		println("Geben sie 1 für A1 ein und 2 für A2");
-		int wahl = readInt();
-		int a;
-
-		if (wahl == 1) {
-			a=a1(n);
-		} else if (wahl == 2) {
-			a=a2(n);
-
-		} else {
-			println("Fehlerhafte eingabe");
-			return;
-		}
-		println("Die Gesuchte Zahl ist: " + a);
-
+println(1e100*1e200);
 	}
 }
