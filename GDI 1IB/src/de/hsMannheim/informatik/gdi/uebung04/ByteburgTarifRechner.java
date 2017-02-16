8497d6b974e8e6f531a2b301f76314af926d2e0c
lange <lange@SURFACE-LANGE>
2017-02-16 08:10:51 +0100

    Biteburg GUI

--- GDI 1IB/src/de/hsMannheim/informatik/gdi/uebung04/ByteburgTarifRechner.java
+++ GDI 1IB/src/de/hsMannheim/informatik/gdi/uebung04/ByteburgTarifRechner.java
@@ -2,7 +2,14 @@ package de.hsMannheim.informatik.gdi.uebung04;
 
 import static gdi.MakeItSimple.*;
 
+import freierTest.GUI_Biteburg;
+
 public class ByteburgTarifRechner {
+	
+	public static int startStation = 0;
+	public static int zielStation = 0 ;
+	public static int fahrtKosten = 0;
+	public static String info = null;
 
 	static boolean istStationInLinie(int station, int[] linie) {
 		int i = 0;
@@ -108,33 +115,37 @@ public class ByteburgTarifRechner {
 	public static void main(String[] args) {
 		int[] stationen = { 00, 11, 12, 13, 14, 15, 16, 21, 22, 23, 24, 25, 26, 31, 32, 33, 34, 35, 36, 41, 42, 43, 44,
 				45, 46, 51, 52, 53, 54, 55, 56/*, 61, 62, 63, 64, 65, 66 */};
-		int startStation;
-		int zielStation;
-		int fahrtKosten;
-		int erneut;
-
-		print("Geben Sie ihre Startstation ein: ");
-		startStation = readInt();
-		while (!istStationInLinie(startStation, stationen)) {
-			print("Die von Ihnen eingegebene Startstation ist nicht verfügbar. Bitte geben Sie nun eine gültige ein: ");
-			startStation = readInt();
-		}
-
-		print("Geben Sie ihre Zielstation ein: ");
-		zielStation = readInt();
-		while (!istStationInLinie(zielStation, stationen)) {
-			print("Die von Ihnen eingegebene Zielstation ist nicht verfügbar. Bitte geben Sie nun eine gültige ein: ");
-			zielStation = readInt();
-		}
-
+		info = "";
+		fahrtKosten = 0;
+		
+		
+		startStation = GUI_Biteburg.start;
+//		while (!istStationInLinie(startStation, stationen)) {
+//			print("Die von Ihnen eingegebene Startstation ist nicht verfügbar. Bitte geben Sie nun eine gültige ein: ");
+//			startStation = readInt();
+//		}
+//
+		zielStation = GUI_Biteburg.ziel;
+//		while (!istStationInLinie(zielStation, stationen)) {
+//			print("Die von Ihnen eingegebene Zielstation ist nicht verfügbar. Bitte geben Sie nun eine gültige ein: ");
+//			zielStation = readInt();
+//		}
+		
+		if(!istStationInLinie(startStation, stationen)||!istStationInLinie(zielStation, stationen)){
+			info = "Start oder Ziel ist ungültig";
+		}		
+		
+		println("Start: " + startStation);
+		println("Ziel: " + zielStation);
 		fahrtKosten = ermittleFahrtkosten(startStation, zielStation);
 		
 		if (fahrtKosten == 0) {
-			println("Sie sind bereits an ihrem Ziel!");
+			info = "Sie sind bereits an ihrem Ziel!";
 		}
 		else if (fahrtKosten > 0)
 			println("Sie müssen für diese Fahrt " + fahrtKosten + " Euro zahlen.");
 
+		
 		println(">>>Programmende<<<");
 
 		// Wenn der Fahrplan um eine weitere Linie ausgebaut werden soll, muss
@@ -142,80 +153,7 @@ public class ByteburgTarifRechner {
 		// 1. in der main-Methode: im array "stationen" muss das Kommentar angepasst werden
 		// 2. in der ermittleFahrtkosten-Methode: im Array "ringLinie" muss das Kommentar angepasst werden
 
-		/*
-		 * // Teste Nachbarstation println("00 und 11 (erwartet 1): " +
-		 * ermittleFahrtkosten(0, 11) + "\n___________________________");
-		 * println("11 und 00 (erwartet 1): " + ermittleFahrtkosten(11, 0) +
-		 * "\n___________________________"); println("11 und 12 (erwartet 1): "
-		 * + ermittleFahrtkosten(11, 12) + "\n___________________________");
-		 * println("12 und 11 (erwartet 1): " + ermittleFahrtkosten(12, 11) +
-		 * "\n___________________________"); println("35 und 36 (erwartet 1): "
-		 * + ermittleFahrtkosten(35, 36) + "\n___________________________");
-		 * println("23 und 33 (erwartet 1): " + ermittleFahrtkosten(23, 33) +
-		 * "\n___________________________"); println("53 und 13 (erwartet 1): "
-		 * + ermittleFahrtkosten(53, 13) + "\n___________________________");
-		 * println("13 und 53 (erwartet 1): " + ermittleFahrtkosten(13, 53) +
-		 * "\n___________________________");
-		 * 
-		 * //Teste normale Fahrten println("23 und 43 (erwartet 2): " +
-		 * ermittleFahrtkosten(23, 43) + "\n___________________________");
-		 * println("43 und 23 (erwartet 2): " + ermittleFahrtkosten(43, 23) +
-		 * "\n___________________________"); println("00 und 13 (erwartet 2): "
-		 * + ermittleFahrtkosten(0, 13) + "\n___________________________");
-		 * println("13 und 00 (erwartet 2): " + ermittleFahrtkosten(13, 0) +
-		 * "\n___________________________"); println("12 und 41 (erwartet 2): "
-		 * + ermittleFahrtkosten(12, 41) + "\n___________________________");
-		 * println("42 und 53 (erwartet 2): " + ermittleFahrtkosten(42, 53) +
-		 * "\n___________________________"); println("53 und 00 (erwartet 2): "
-		 * + ermittleFahrtkosten(53, 00) + "\n___________________________");
-		 * println("11 und 41 (erwartet 2): " + ermittleFahrtkosten(11, 41) +
-		 * "\n___________________________");
-		 * 
-		 * //Teste schonDa println("00 und 00 (erwartet 0): " +
-		 * ermittleFahrtkosten(0, 0) + "\n___________________________");
-		 * println("11 und 11 (erwartet 0): " + ermittleFahrtkosten(11, 11) +
-		 * "\n___________________________"); println("55 und 55 (erwartet 0): "
-		 * + ermittleFahrtkosten(55, 55) + "\n___________________________");
-		 * println("34 und 34 (erwartet 0): " + ermittleFahrtkosten(34, 34) +
-		 * "\n___________________________"); println("35 und 35 (erwartet 0): "
-		 * + ermittleFahrtkosten(35, 35) + "\n___________________________");
-		 * println("33 und 33 (erwartet 0): " + ermittleFahrtkosten(33, 33) +
-		 * "\n___________________________"); println("31 und 31 (erwartet 0): "
-		 * + ermittleFahrtkosten(31, 31) + "\n___________________________");
-		 * println("44 und 44 (erwartet 0): " + ermittleFahrtkosten(44, 44) +
-		 * "\n___________________________");
-		 * 
-		 * //Teste Zonenübergreifend println("00 und 24 (erwartet 3): " +
-		 * ermittleFahrtkosten(0, 24) + "\n___________________________");
-		 * println("24 und 00 (erwartet 3): " + ermittleFahrtkosten(24, 0) +
-		 * "\n___________________________"); println("23 und 25 (erwartet 3): "
-		 * + ermittleFahrtkosten(23, 25) + "\n___________________________");
-		 * println("25 und 23 (erwartet 3): " + ermittleFahrtkosten(25, 23) +
-		 * "\n___________________________"); println("11 und 35 (erwartet 3): "
-		 * + ermittleFahrtkosten(11, 35) + "\n___________________________");
-		 * println("35 und 11 (erwartet 3): " + ermittleFahrtkosten(35, 11) +
-		 * "\n___________________________"); println("54 und 13 (erwartet 3): "
-		 * + ermittleFahrtkosten(54, 13) + "\n___________________________");
-		 * println("13 und 54 (erwartet 3): " + ermittleFahrtkosten(13, 54) +
-		 * "\n___________________________");
-		 * 
-		 * //Teste Endstation println("55 und 56 (erwartet 1): " +
-		 * ermittleFahrtkosten(55, 56) + "\n___________________________");
-		 * println("56 und 55 (erwartet 1): " + ermittleFahrtkosten(56, 55) +
-		 * "\n___________________________"); println(
-		 * "54 und 56 (erwartet 2 + Hinweis): " + ermittleFahrtkosten(54, 56) +
-		 * "\n___________________________"); println("56 und 54 (erwartet 2): "
-		 * + ermittleFahrtkosten(56, 54) + "\n___________________________");
-		 * println("00 und 16 (erwartet 4): " + ermittleFahrtkosten(0, 16) +
-		 * "\n___________________________"); println("16 und 00 (erwartet 3): "
-		 * + ermittleFahrtkosten(16, 0) + "\n___________________________");
-		 * println("43 und 56 (erwartet 4): " + ermittleFahrtkosten(43, 56) +
-		 * "\n___________________________"); println("13 und 54 (erwartet 3): "
-		 * + ermittleFahrtkosten(13, 54) + "\n___________________________");
-		 * println("55 und 46 (erwartet 5): " + ermittleFahrtkosten(55, 46) +
-		 * "\n___________________________"); println("46 und 55 (erwartet 4): "
-		 * + ermittleFahrtkosten(46, 55) + "\n___________________________");
-		 */
+		
 
 	}
 }
--- GDI 1IB/src/freierTest/JanneckTest.java
+++ GDI 1IB/src/freierTest/JanneckTest.java
@@ -1,5 +1,7 @@
 package freierTest;
-
+/*
+ *Performencetest per Priemzahlen
+ */
 import java.util.ArrayList;
 import java.util.List;
 
