41a2291724245cdd6f1de9cec230de708399522c
Benjamin <Benjamin@192.168.178.44>
2017-04-05 18:12:07 +0200

    Verbesserungen eingepflegt

--- de.hsMannheim.informatik.tpe.ss17.gruppe26/src/de/hsMannheim/informatik/tpe/ss17/gruppe26/uebung01/Menue.java
+++ de.hsMannheim.informatik.tpe.ss17.gruppe26/src/de/hsMannheim/informatik/tpe/ss17/gruppe26/uebung01/Menue.java
@@ -25,40 +25,48 @@ public class Menue {
 		System.out.println("Programm gestartet. (100%)");
 
 		System.out.println("Was wollen Sie machen?");
-		System.out.println("(1) Werte manuell einfügen");
+		System.out.println("(1) Werte in einen  einfügen");
 		System.out.println("(2) Werte über Datei einfügen");
 		System.out.println("(3) Wert finden");
 		System.out.println("(4) Anzahl der Werte zählen lassen");
 		System.out.println("(5) Höhe des Baumes ausgeben lassen");
 		System.out.println("(6) Größtes Element ausgeben lassen");
-		System.out.println("(7) Kleinestes Element ausgeben lassen");
+		System.out.println("(7) Kleinstes Element ausgeben lassen");
 		switch (readInt()) {
 		case (1):
 			tree.insert(5);
 			tree.insert(3);
-			tree.insert(4);
+//			tree.insert(4);
 			 break;
 		case (2):// insert(file);
 			// break;
 		case (3): // contains(integer);
 			// break;
 		case (4):
-			// tree.size();
+			int size = 0;
+			size = tree.size();
+			if (size == 0)
+				System.out.println("Es gibt kein Element im Baum!");
+			System.out.println("[size] Im B-Baum gibt es " + size + "Elemente.");
 			// break;
 		case (5)://
 			int height = 0;
 			height = tree.height();
-			System.out.println("Die Höhe des B-Baumes ist: " + height);
+			System.out.println("[height] Die Höhe des B-Baumes ist: " + height);
 			// break;
 		case (6):
 			Integer maxVal = null;
 			maxVal = tree.getMax();
-			System.out.println("Der größte Wert ist: " + maxVal);
+			if (maxVal == null)
+				System.out.println("Es gibt kein größtes Element!");
+			System.out.println("[getMax] Der größte Wert ist: " + maxVal);
 			// break;
 		case (7):
 			Integer minVal = null;
 			minVal = tree.getMin();
-			System.out.println("Der kleinste Wert ist: " + minVal);
+			if (minVal == null)
+				System.out.println("Es gibt kein kleinstes Element!");
+			System.out.println("[getMin] Der kleinste Wert ist: " + minVal);
 			break;
 		// TODO alles weitere
 		}
