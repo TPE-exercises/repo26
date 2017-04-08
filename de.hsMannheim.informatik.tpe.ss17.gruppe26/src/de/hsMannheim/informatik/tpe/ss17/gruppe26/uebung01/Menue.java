74534c953bbe0d84dbe1c0061d4a89eec5cc9838
1622998 <Benjamin@192.168.178.44>
2017-04-08 14:01:48 +0200

    Neue Zahlen

--- de.hsMannheim.informatik.tpe.ss17.gruppe26/src/de/hsMannheim/informatik/tpe/ss17/gruppe26/uebung01/Menue.java
+++ de.hsMannheim.informatik.tpe.ss17.gruppe26/src/de/hsMannheim/informatik/tpe/ss17/gruppe26/uebung01/Menue.java
@@ -49,13 +49,24 @@ public class Menue {
 				switch (readInt()) {
 				case (1): // TODO manuelle Eingabe ermöglichen
 					System.out.println("Werte werden in den Baum eingefügt...");
+					tree.insert(0);
+					tree.insert(1);
+					tree.insert(2);
+					tree.insert(3);
+					tree.insert(4);
 					tree.insert(5);
 					tree.insert(6);
 					tree.insert(7);
-					tree.insert(4);
-					tree.insert(3);
 					tree.insert(8);
-//					tree.insert(9);
+					tree.insert(9);
+					tree.insert(10);
+					tree.insert(11);
+					tree.insert(17);
+					tree.insert(1);
+					tree.insert(21);
+					tree.insert(24);
+					tree.insert(17);
+					tree.insert(0);
 
 					
 					break;
