5bf30006f906dd20ece5d20ec8e240b05ba38bfe
1628574 <1628574@stud.hs-mannheim.de>
2017-04-19 22:20:55 +0200

    + Aufgabe 2 +Grundgerüsst für alle Sorten

---
+++ de.hsMannheim.informatik.tpe.ss17.gruppe26/src/uebung02/Eis.java
@@ -0,0 +1,159 @@
+package uebung02;
+
+import static gdi.MakeItSimple.*;
+
+public class Eis {
+
+	private String name;
+	private double preis;
+	private String behaeltnis;
+	private String art;
+	private String[] sorten;
+	private String[] extras;
+
+	Eis() {
+		this.name = null;
+		this.preis = 0;
+		this.behaeltnis = null;
+		this.art = null;
+		this.sorten = null;
+		this.extras = null;
+	}
+
+	Eis(String name, double preis, String behaeltnis, String art, String[] sorten, String[] extras) {
+		this.name = name;
+		this.preis = preis;
+		this.behaeltnis = behaeltnis;
+		this.art = art;
+		this.sorten = sorten;
+		this.extras = extras;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+
+	public void setPreis(double preis) {
+		this.preis = preis;
+	}
+	
+	public double getPreis(){
+		return this.preis;
+	}
+
+	public void setBehaeltnis(String behaeltnis) {
+		this.behaeltnis = behaeltnis;
+	}
+	
+	public String getBehaeltnis(){
+		return this.behaeltnis;
+	}
+
+	public void setArt(String art) {
+		this.art = art;
+	}
+	
+	public String getArt(){
+		return this.art;
+	}
+
+	public void setSorten(String[] sorten) {
+		this.sorten = sorten;
+	}
+	
+	public String[] getSorten(){
+		return this.sorten;
+	}
+
+	public void setExtras(String[] extras) {
+		this.extras = extras;
+	}
+	
+	public String[] getExtras(){
+		return this.extras;
+	}
+
+	// Spaghettieis
+	/*
+	 * Schale Schlagsahne über die Vanilleeis gepresst wird Graniert mit
+	 * Erdbeersoße und geraspelter weißer Schokolade oder Kokosflocken
+	 */
+
+	// Bananensplit
+	/*
+	 * Banane, darauf Kugeln Vanille-, Schokoladen- und Erdbeereis darüber
+	 * Ananassirup, Schokoladensauce und Erdbeersirup topping: Nusssplitter,
+	 * Schlagsahne und Maraschino-Kirschen.
+	 */
+
+	// Nussbecher
+	/*
+	 * 3 Kugeln Eis, Nüsse, Sahne und Nusssauce
+	 */
+
+	/**
+	 * Behaeltnis nehmen. . . Waffel/Becher/Teller/Schussel/Glas/etc.
+	 */
+	public void vorbereiten() {
+		switch (this.name) {
+		case ("Spaghettieis"):
+		case ("Bananensplit"):
+			this.setBehaeltnis("Teller");
+			break;
+		case ("Nussbecher"):
+			this.setBehaeltnis("Becher");
+			break;
+		}
+	}
+
+	/**
+	 * Behaeltnis mit Sorten nach Art fuellen. . .
+	 */
+	public void fuellen() {
+		switch (this.name) {
+		case ("Spaghettieis"):
+			this.setArt("Spaghettis");
+			String[] sorten = { "Vanille", "Vanille", "Vanille" };
+			this.setSorten(sorten);
+			break;
+		case ("Bananensplit"):
+
+			break;
+		case ("Nussbecher"):
+
+			break;
+		}
+	}
+
+	/**
+	 * 
+	 * @return String array
+	 */
+	private String[] frageSortenAb() {
+
+		return null;
+	}
+
+	/**
+	 * Eis mit Extras dekorieren...
+	 */
+	public void dekorieren() {
+		switch (this.name) {
+		case ("Spaghettieis"):
+			String[] extras = { "Erdbeersoße", "Kokosflocken" };
+			this.setExtras(extras);
+			break;
+		case ("Bananensplit"):
+
+			break;
+		case ("Nussbecher"):
+
+			break;
+		}
+	}
+
+}
--- de.hsMannheim.informatik.tpe.ss17.gruppe26/src/uebung02/Menue.java
+++ de.hsMannheim.informatik.tpe.ss17.gruppe26/src/uebung02/Menue.java
@@ -3,8 +3,8 @@ package uebung02;
 public class Menue {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Eisdiele firstEisdiele = new Eisdiele("EisBest", "Käfertal");
+		firstEisdiele.bestellen();
 	}
 
 }
