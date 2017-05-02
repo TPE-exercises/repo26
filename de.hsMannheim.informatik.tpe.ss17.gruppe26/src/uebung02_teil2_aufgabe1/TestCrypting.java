732eec4c237060af81f65455eef7ccbcbcaba08f
1622998 <Benjamin@SurfacePro4-Ben.fritz.box>
2017-05-02 20:29:48 +0200

    Verlangte Verschlüsselungsfunktionen stehen

--- de.hsMannheim.informatik.tpe.ss17.gruppe26/src/uebung02_teil2_aufgabe1/TestCrypting.java
+++ de.hsMannheim.informatik.tpe.ss17.gruppe26/src/uebung02_teil2_aufgabe1/TestCrypting.java
@@ -1,19 +1,18 @@
 package uebung02_teil2_aufgabe1;
 
-import static org.junit.Assert.*;
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
 
-public class TestCaesar {
+import org.junit.Test;
+
+public class TestCrypting {
 
 	@Test
 	public void encryption() {
 		Crypter caesarLetters = new CrypterCaesar(0);
 		assertEquals("abc: ", "ABC", caesarLetters.encrypt("abc"));
 		assertEquals("qwertzuiop: ", "QWERTZUIOP", caesarLetters.encrypt("qwertzuiop"));
-		
 		caesarLetters = new CrypterCaesar(1);
 		assertEquals("abc: ", "BCD", caesarLetters.encrypt("abc"));
-
 	}
 	
 	@Test
@@ -21,7 +20,6 @@ public class TestCaesar {
 		Crypter caesarLetters = new CrypterCaesar(0);
 		assertEquals("ABC: ", "abc", caesarLetters.decrypt("ABC"));
 		assertEquals("QWERTZUIOP: ", "qwertzuiop", caesarLetters.decrypt("QWERTZUIOP"));
-		
 	}
-
+	
 }
