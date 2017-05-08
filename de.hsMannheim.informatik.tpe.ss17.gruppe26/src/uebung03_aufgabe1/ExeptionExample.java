7612bb36eeed8a6526dae2bf4b0a67facbe73382
1628574 <1628574@stud.hs-mannheim.de>
2017-05-08 09:04:04 +0200

    Klasse in 2. Version fertig zur abgabe

--- de.hsMannheim.informatik.tpe.ss17.gruppe26/src/uebung03_aufgabe1/ExeptionExample.java
+++ de.hsMannheim.informatik.tpe.ss17.gruppe26/src/uebung03_aufgabe1/ExeptionExample.java
@@ -5,22 +5,43 @@ import static gdi.MakeItSimple.*;
 public class ExeptionExample {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Version 1.8 (09/04/2017)");
-		System.out.println("Erstellt von: Schoenke und Lange");
-		System.out.println("1. Programmieraufgabe \"BTree\" aus TPE im SS17");
-		printSeperatorLine();
-		
+
+		System.out.println("Version 2 (08/05/2017)");
+		System.out.println("Erstellt von: Lange");
+		System.out.println("3. Programmieraufgabe \"Exceptions - Theorie\" aus TPE im SS17");
+		OwnUtils.printLines.printSeperatorLine();
+
+		div(enterNumbers());
+
+	}
+
+	/**
+	 * enter two int values
+	 * @return array of int / length of 2
+	 */
+	private static int[] enterNumbers() {
+		int[] numbers = new int[2];
 		System.out.print("Geben Sie die erste Zahl ein: ");
-		int numberOne=readInt();
+		numbers[0] = readInt();
 		System.out.print("Geben Sie die zweite Zahl ein: ");
-		int numberSec=readInt();
-		
-		try{
-			System.out.println(numberOne/numberSec);
-		}catch(ArithmeticException ex){
+		numbers[1] = readInt();
+		return numbers;
+	}
+
+	/**
+	 * Division of two numbers from an array
+	 * Exception division by zero is catched
+	 * @param numbers
+	 */
+	private static void div(int[] numbers) {
+		int sum=-1;
+		try {
+			sum = numbers[0] / numbers[1];
+			System.out.println(sum);
+		} catch (ArithmeticException ex) {
 			System.err.println("Sie dürfen um gottes Willen nicht durch NULL dividieren!");
 		}
+		
 	}
 
 }
